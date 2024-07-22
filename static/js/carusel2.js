0f44e689959f370b6c0785c67a09dcec524b1a4e
unknown <netolivehappy@gmail.com>
2024-07-22 17:15:13 -0400

    update main image product detail

--- static/js/carusel2.js
+++ static/js/carusel2.js
@@ -1,57 +1,48 @@
 document.addEventListener('DOMContentLoaded', function () {
-  //alert ('hellow carrusel')
-
   const carousel = document.querySelector('#productoCarousel');
   const fullScreenImg = document.querySelector('#fullScreenImg');
   const fullScreenContainer = document.querySelector('#fullScreenImage');
   const closeBtn = document.querySelector('#closeFullScreen');
   const imageNumber = document.querySelector('#imageNumber');
   const totalImages = document.querySelector('#totalImages');
-
   const prevControl = document.querySelector('.carousel-control-prev');
   const nextControl = document.querySelector('.carousel-control-next');
 
-
-  // Actualizar el contador de imágenes
   function updateImageCounter() {
     const currentIndex = $(carousel).find('.carousel-item.active').index() + 1;
     imageNumber.textContent = currentIndex;
   }
 
   function updateControls() {
-
     const currentIndex = $(carousel).find('.carousel-item.active').index();
     const totalItems = $(carousel).find('.carousel-item').length;
-
-    console.log('currentIndex:' + currentIndex + ' / totalItems: ' + totalItems)
 
     if (currentIndex === 0) {
       prevControl.classList.add('disabled');
       prevControl.setAttribute('aria-disabled', 'true');
-      console.log('logica prevControl true:' + currentIndex)
     } else {
       prevControl.classList.remove('disabled');
-      console.log('logica prevControl false:' + currentIndex)
+      prevControl.removeAttribute('aria-disabled');
     }
 
     if (currentIndex === totalItems - 1) {
       nextControl.classList.add('disabled');
       nextControl.setAttribute('aria-disabled', 'true');
-      console.log('logica nextControl true:' + currentIndex)
     } else {
       nextControl.classList.remove('disabled');
-      console.log('logica nextControl false:' + currentIndex)
+      nextControl.removeAttribute('aria-disabled');
     }
   }
 
+  $(carousel).on('slide.bs.carousel', function () {
+    updateImageCounter();
+    updateControls();
+  });
 
-  //$(carousel).on('click', '.carousel-control-prev, .carousel-control-next', function () {
+  updateImageCounter();
+  updateControls();
+
   $(carousel).on('click', '.carousel-control-prev, .carousel-control-next', function (e) {
-    /*setTimeout(function () {
-      updateImageCounter();
-      updateControls();
-    }, 500); // Ajusta el tiempo si es necesario
-*/
     if (this.classList.contains('disabled')) {
       e.preventDefault();
       e.stopPropagation();
@@ -63,62 +54,26 @@ document.addEventListener('DOMContentLoaded', function () {
     }
   });
 
-
-  $(carousel).on('slide.bs.carousel', function () {
-    updateImageCounter();
-  });
-
-  updateImageCounter();
-
-  // Abrir imagen en pantalla completa
   $(carousel).on('click', '.carousel-item img', function () {
     fullScreenImg.src = this.src;
-    fullScreenContainer.style.display = 'block';
+    fullScreenContainer.style.display = 'flex';
   });
 
-  // Cerrar pantalla completa
   closeBtn.addEventListener('click', function () {
     fullScreenContainer.style.display = 'none';
   });
 
-  // Navegación por deslizamiento en móviles
   let touchStartX = 0;
   let touchEndX = 0;
-
-  //La función handleSwipe detecta la dirección del deslizamiento (izquierda o derecha) y mueve el carrusel en consecuencia si los controles correspondientes no están deshabilitados.
-
-  /*function handleSwipe() {
-    const currentIndex = $(carousel).find('.carousel-item.active').index();
-    const totalItems = $(carousel).find('.carousel-item').length;
-
-    if (touchEndX < touchStartX && currentIndex < totalItems - 1) {
-      if (!nextControl.classList.contains('disabled')) {
-        $(carousel).carousel('next');
-      }
-    } else if (touchEndX > touchStartX && currentIndex > 0) {
-      if (!prevControl.classList.contains('disabled')) {
-        $(carousel).carousel('prev');
-      }
-    }
-    setTimeout(function () {
-      updateImageCounter();
-      updateControls();
-    }, 500);
-  }*/
-
 
   function handleSwipe() {
     const currentIndex = $(carousel).find('.carousel-item.active').index();
     const totalItems = $(carousel).find('.carousel-item').length;
 
     if (window.innerWidth < 768) { // Para dispositivos pequeños
-      // A la izquierda 
       if (touchEndX < touchStartX && currentIndex < totalItems - 1) {
-        alert('izquierda > next - currentIndex:' + currentIndex + ' totalItems-1:' + (totalItems - 1))
         $(carousel).carousel('next');
-        //A la derecha
       } else if (touchEndX > touchStartX && currentIndex > 0) {
-        alert('derecha > prev - currentIndex:' + currentIndex)
         $(carousel).carousel('prev');
       }
     } else { // Para dispositivos grandes
@@ -133,7 +88,6 @@ document.addEventListener('DOMContentLoaded', function () {
       updateImageCounter();
       updateControls();
     }, 500); // Ajusta el tiempo si es necesario
-
   }
 
   carousel.addEventListener('touchstart', function (event) {
@@ -145,13 +99,7 @@ document.addEventListener('DOMContentLoaded', function () {
     handleSwipe();
   });
 
-
-  // Desactivar el deslizamiento automático
-  //$(carousel).carousel('dispose');
-
-  // Desactivar el deslizamiento automático
   $(carousel).carousel({
     interval: false // Establece el intervalo en false para desactivar el deslizamiento automático
   });
 });
-
